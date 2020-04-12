60e36f5afb6bddc2b95537320eb2514c39d60be1
pinguinul87 <42859236+pinguinul87@users.noreply.github.com>
2020-04-12 09:42:28 +0300

    Create runCommandBasedOnUser.sh

---
+++ runCommandBasedOnUser.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+: '
+This script will run a given command via the command line as the first argument, based on the privileges of a given user.
+Useage : ./runCommandBasedOnUser.sh root ls -l
+
+uid=$(cat /etc/passwd | grep $1 | cut -d : -f 3)
+cmd=$2
+ 
+find . -user $uid -exec $cmd {} \;
+ 

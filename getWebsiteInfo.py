4cd11904402033cfb4649845c0b5c50d394ffade
pinguinul87 <42859236+pinguinul87@users.noreply.github.com>
2020-04-12 11:38:33 +0300

    Create getWebsiteInfo.py

---
+++ getWebsiteInfo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This script will return info from a given domanin.
+Will return the location, city, country and IP address beased on the data stored on ipinfo.io
+"""
+
+import sys
+import json
+import socket
+import requests
+
+#How to use the script
+if len(sys.argv) < 2:
+    print("Use script like this: " + sys.argv[0] + "URL")
+    sys.exit(1)
+
+#Make request and print http header info
+rqst_one = requests.get("https://"+sys.argv[1])
+print("\n"+str(rqst_one.headers))
+
+#Get IP address
+getIP = socket.gethostbyname(sys.argv[1])
+print("\nThe IP address of "+sys.argv[1]+" is: "+getIP + "\n")
+
+rqst_two = requests.get("https://ipinfo.io/"+getIP +"/json")
+resp = json.loads(rqst_two.text)
+
+#List information regarding domain location
+print("The city is : "    + resp["city"])
+print("The country is : " + resp["country"])
+print("The region is : "  + resp["region"])
+print("The location is : "+ resp["loc"])
+
+
